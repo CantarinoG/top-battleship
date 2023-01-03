4beadbedccf9a24a1bd232125ddee037b3eadb04
CantarinoG <guilhermecantarino1709@gmail.com>
2023-01-03 17:32:26 -0300

    Add design for the board setting stage

--- src/scripts/UI/preparationUI.js
+++ src/scripts/UI/preparationUI.js
@@ -1,5 +1,31 @@
+import '../../styles/preparation.css';
+
 export function renderPreparation() {
     const body = document.querySelector('body');
     body.innerHTML = `
+    <div id="preparation">
+        <header>
+            <h1>PLACE YOUR FLEET...</h1>
+        </header>
+        <main>
+            <div id="board">
+            </div>
+        </main>
+    </div>
     `;
+
+    const board = document.getElementById("board");
+    createBoard(board);
 }
+
+function createBoard(board) {
+    let boardHtml = '';
+    for (let i = 0; i < 10; i += 1) {
+      boardHtml += "<div class='row'>";
+      for (let j = 0; j < 10; j += 1) {
+        boardHtml += `<div class='column'></div>`;
+      }
+      boardHtml += '</div>';
+    }
+    board.innerHTML = boardHtml;
+  }
